5c322bd74ad5b2b2bec20896955789f68ac08515
KevinKipkemei <42734616+KevinKipkemei@users.noreply.github.com>
2023-07-17 00:26:45 +0300

    Deploy Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import Github from '../src/assets/logo-github.svg';
 import LinkedIn from '../src/assets/logo-linkedin.svg';
